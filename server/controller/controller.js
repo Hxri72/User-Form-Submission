88a2992425c95e8143df1f2d3f01884f987c69f7
Hariprasad K H <hariprasad72172@gmail.com>
2023-05-07 21:48:10 +0530

    some changes done

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -6,6 +6,38 @@ module.exports = {
             const userData = req.body
             const userExist = await userModel.find({name:userData.name})
             
+            const idArray = {
+                ID:userData.ID,
+                govtId:userData.govtId
+            }
+
+            const guardianArr = {
+                label : userData.label,
+                guardianName : userData.guardianName
+            }
+            
+            userDataModified = {
+                name:userData.name,
+                age:userData.age,
+                sex:userData.sex,
+                mobile:userData.mobile,
+                id:idArray,
+                guardian:guardianArr,
+                email:userData.email,
+                emergencyContact:userData.emergencyContact,
+                address:userData.address,
+                state:userData.state,
+                city:userData.city,
+                country:userData.country,
+                pincode:userData.pincode,
+                occupation:userData.occupation,
+                religion:userData.religion,
+                marital:userData.marital,
+                bloodGroup:userData.bloodGroup,
+                nationality:userData.nationality
+            }
+
+            
             if(userExist.length > 0){
                return res.send({
                     success:false,
@@ -14,7 +46,7 @@ module.exports = {
             }
             
 
-            const newUser = new userModel(userData);
+            const newUser = new userModel(userDataModified);
             await newUser.save();
             console.log('saved');
 
